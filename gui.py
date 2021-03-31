f706ad2f5fd175117f3b90c1dba4f50d6d5cc645
maxjay <disgracedandbetrayed@gmail.com>
2021-03-31 22:04:34 +0100

    update

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ from ant import Ant
 
 
 ants = [Ant() for i in range(50)]
-camera = (250, 250)
+camera = [250, 250]
 
 pygame.init()
 screen = pygame.display.set_mode([500, 500])
@@ -12,8 +12,11 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_w:
+                print("hello") 
+                camera[0] -= 1
     screen.fill((255, 255, 255))
-    # Draw a solid blue circle in the center
     for i in ants:
         pygame.draw.circle(screen, (0, 0, 255), (i.x + camera[0], i.y + camera[1]), 1)
         i.update()
